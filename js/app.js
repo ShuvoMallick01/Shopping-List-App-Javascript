3f0edb445f2aedb284de98f13b8caf83a849648f
ShuvoMallick01 <shuvacse01@gmail.com>
2023-03-19 16:43:09 +0600

    First Commit Shopping List

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const itemList = document.querySelector(".item-list");
+const items = document.querySelectorAll("li");
+const clrBtn = document.querySelector(".btn-clear");
+
+// clrBtn.onclick = () => {
+//   console.log("Clear Btn");
+// };
+
+clrBtn.onclick = function () {
+  console.log("Clear Btn");
+};
+// clrBtn.addEventListener("click", (e) => {
+//   alert("clear All");
+// });
+
+// function onClick() {
+//   alert("Clear All");
+// }
+// clrBtn.onclick = onClick;
